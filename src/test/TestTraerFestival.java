0f896a471545df41202abb821cf4439d67e70f61
Emiliano <emilianokihn323@gmail.com>
2026-08-29 19:22:06 -0300

    Implemento el abm + tests de traer

---
+++ src/test/TestTraerFestival.java
@@ -0,0 +1,22 @@
+package test;
+
+import datos.Festival;
+import negocio.FestivalABM;
+
+public class TestTraerFestival {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FestivalABM abm = new FestivalABM();
+		System.out.println("Test traer todos los festivales:\n");
+		for (Festival f: abm.traer()) {
+			System.out.println(f);
+		}
+		System.out.println("Traer festival con id 1:\n");
+		System.out.println(abm.traer(1));
+		System.out.println("Traer todas las unidades de venta del festival 1:\n");
+		System.out.println(abm.traerFestivalYUnidad(1));
+		
+	}
+
+}
