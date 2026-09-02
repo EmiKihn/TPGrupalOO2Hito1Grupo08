23a239ba4b20ee78a5008f2897750af8dae4ea89
Emiliano <emilianokihn323@gmail.com>
2026-09-02 16:35:34 -0300

    Cambio estructura a Singleton

--- src/dao/DetallePedidoDao.java
+++ src/dao/DetallePedidoDao.java
@@ -10,7 +10,6 @@ import org.hibernate.query.Query;
 
 import datos.DetallePedido;
 
-
 public class DetallePedidoDao {
 
 	private static Session session;
@@ -27,23 +26,38 @@ public class DetallePedidoDao {
 	}
 	
 	public DetallePedido traer(long idDetallePedido) {
-		DetallePedido objeto = null;
-		try {
-		iniciaOperacion();
-		objeto = (DetallePedido) session.get(DetallePedido.class, idDetallePedido);
-		}catch(HibernateException he) {
-			manejaExcepcion(he);
-		} finally {
-		session.close();
-		}
-		return objeto;
-		}
+	    DetallePedido objeto = null;
+	    try {
+	        iniciaOperacion();
+
+	        objeto = session.createQuery(
+	            "from DetallePedido d " +
+	            "join fetch d.pedido " +
+	            "join fetch d.plato " +
+	            "where d.idDetallePedido = :id",
+	            DetallePedido.class
+	        )
+	        .setParameter("id", idDetallePedido)
+	        .uniqueResult();
+
+	    } catch(HibernateException he) {
+	        manejaExcepcion(he);
+	    } finally {
+	        session.close();
+	    }
+	    return objeto;
+	}
 	
 	public List<DetallePedido> traer() {
 		List<DetallePedido> lista = new ArrayList<DetallePedido>();
 		try {
 		iniciaOperacion();
-		Query<DetallePedido> query = session.createQuery("from DetallePedido d", DetallePedido.class);
+		Query<DetallePedido> query = session.createQuery(
+			    "from DetallePedido d " +
+			    "join fetch d.pedido " +
+			    "join fetch d.plato",
+			    DetallePedido.class
+			);
 		lista = query.getResultList();
 		} catch (HibernateException he) {
 			manejaExcepcion(he);
--- src/test/TestTraerPlato.java
+++ src/test/TestTraerPlato.java
@@ -1,6 +1,10 @@
 package test;
 
+import java.util.List;
+
+import datos.Pedido;
 import datos.Plato;
+import negocio.PedidoABM;
 import negocio.PlatoABM;
 
 public class TestTraerPlato {
@@ -8,18 +12,17 @@ public class TestTraerPlato {
 	public static void main(String[] args) {
 
 		
-		PlatoABM abm = new PlatoABM();
 		System.out.println("Test traer todos los platos:\n");
-		for (Plato p: abm.traer()) {
+		for (Plato p: PlatoABM.getInstancia().traer()) {
 			System.out.println(p);
 		}
 		
 		System.out.println("Traer plato con id 1:\n");
-		System.out.println(abm.traer(1));
+		System.out.println(PlatoABM.getInstancia().traer(1));
 		
 		String nombre = "nombre";
 		System.out.println("Traer el plato " + nombre + ":\n");
-		System.out.println(abm.traerPorNombre(nombre));
+		System.out.println(PlatoABM.getInstancia().traerPorNombre(nombre));
 		
 	}
 
