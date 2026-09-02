2da989c871452cc79bde4d9371267ba7728950bc
szapajko <164340841+szapajko@users.noreply.github.com>
2026-09-02 15:40:57 -0300

    Actualizacion a clase PedidoDao/ABM y TestTraerPedido

--- src/negocio/PedidoABM.java
+++ src/negocio/PedidoABM.java
@@ -7,18 +7,20 @@ import dao.PedidoDao;
 import datos.Pedido;
 
 public class PedidoABM {
+	private static PedidoABM instancia= null;
+	
+	public static PedidoABM getInstancia() {
+		if(instancia==null) {
+			instancia=new PedidoABM();
+		}
+		return instancia;
+	}
 
-	PedidoDao dao = new PedidoDao();
-	
 	public Pedido traer(long idPedido) {
-		return dao.traer(idPedido);
+		return PedidoDao.getInstancia().traer(idPedido);
 	}
 	
-	public List<Pedido> traer() {
-		return dao.traer();
-	}
-	
-	public Pedido traerPorFecha(LocalDate fechaTransaccion) {
-		return dao.traerPorFecha(fechaTransaccion);
+	public List<Pedido> traer(LocalDate fechaDesde, LocalDate fechaHasta) {
+		return PedidoDao.getInstancia().traer(fechaDesde, fechaHasta);
 	}
 }
--- src/test/TestTraerPedido.java
+++ src/test/TestTraerPedido.java
@@ -1,27 +1,47 @@
 package test;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import datos.DetallePedido;
 import datos.Pedido;
 import negocio.PedidoABM;
 
 public class TestTraerPedido {
 
 	public static void main(String[] args) {
+		LocalDate fechaDesde= LocalDate.of(2026, 9, 20);
+		LocalDate fechaHasta= LocalDate.of(2026, 9, 25);
+		List<Pedido> lista= PedidoABM.getInstancia().traer(fechaDesde, fechaHasta);
+		//ENCABEZADO
+		System.out.println("\n==================================================");
+		System.out.println("              REPORTE DE PEDIDOS                   ");
+		System.out.println("==================================================");
 		
-		PedidoABM abm = new PedidoABM();
-		System.out.println("Test traer todos los pedidos:\n");
-		for (Pedido p: abm.traer()) {
-			System.out.println(p);
+		for (Pedido p : lista) {
+
+		    System.out.printf("TICKET NRO: %04d | FECHA: %s\n", p.getIdPedido(), p.getFechaTransaccion());
+		    System.out.printf("PUESTO: %s\n", p.getUnidadDeVenta().getNombre()); 
+		    System.out.printf("CAJERO/A: %s %s\n", p.getCajero().getNombre(), p.getCajero().getApellido());
+		    
+		    System.out.println("--------------------------------------------------");
+		    System.out.printf("%-5s | %-25s | %-10s\n", "CANT", "PLATO", "SUBTOTAL");
+		    System.out.println("--------------------------------------------------");
+			double total=0.0;
+			Double subtotal=0.0;
+			//CUERPO
+		    for (DetallePedido detalle : p.getDetalles()) {
+		    	subtotal= detalle.getCantidad() * detalle.getPrecioDeVenta();
+		        total += detalle.getCantidad() * detalle.getPrecioDeVenta();
+		        System.out.printf("%-5d | %-25s | $%.2f\n", 
+		                          detalle.getCantidad(), 
+		                          detalle.getPlato().getNombre(), 
+		                          subtotal);
+		    }
+		    //PIE
+		    System.out.println("--------------------------------------------------");
+		    System.out.printf("%-33s   $%.2f\n", "TOTAL A PAGAR: ", total);
+		    System.out.println("==================================================\n");
 		}
-		System.out.println("Traer pedido con id 1:\n");
-		System.out.println(abm.traer(1));
-		
-		LocalDate fecha = LocalDate.now();
-		System.out.println("Traer pedidos de la fecha actual:\n");
-		System.out.println(abm.traerPorFecha(fecha));
-		
 	}
-
-
 }
